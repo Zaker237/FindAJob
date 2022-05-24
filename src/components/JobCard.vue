5163a41dfba9ddcd17b110ed5971f7d16d31e49e
ln-dev7 <leonelngoya@gmail.com>
2022-05-24 09:02:06 +0100

    add firestore with npm

--- src/components/JobCard.vue
+++ src/components/JobCard.vue
@@ -1,13 +1,18 @@
 <template>
   <router-link to="/Job/uid" custom v-slot="{ navigate }">
     <div class="job" @click="navigate">
-      
+
       <button>Postuler</button>
     </div>
   </router-link>
 </template>
 
 <script>
+// import { doc, onSnapshot } from "firebase/firestore";
+
+// const unsub = onSnapshot(doc(db, "cities", "SF"), (doc) => {
+//   console.log("Current data: ", doc.data());
+// });
 export default {
   name: "JobCard",
   data() {
